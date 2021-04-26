80a5c1be057dd5e27868dc34ab15dff9e7e7e67a
Henrique Assis <henrique.assis145@gmail.com>
2021-04-26 16:01:01 -0300

    lifecycle hooks

---
+++ conceitos/6-life cycle hooks/nova-syntax-e-antiga/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Life cycle hooks</h1>
+</template>
+
+<script>
+import {onMounted} from 'vue'
+
+    export default{
+        setup(){
+            onMounted(()=>{
+                console.log('Foi Montado composition API')
+            })
+        }
+    }
+// export default {
+//     mounted(){
+//         console.log('Foi montado!')
+//     }
+// }
+</script>
+
+<style>
+
+</style>
