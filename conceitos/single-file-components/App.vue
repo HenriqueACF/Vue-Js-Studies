43f4b6370a7fca64a57a99d30a9b1afc9bca2f74
Henrique Assis <henrique.assis145@gmail.com>
2021-04-26 13:18:34 -0300

    single file components

---
+++ conceitos/single-file-components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    //Corpo do projeto
+    <h1>Hello World</h1>
+</template>
+
+<script>
+    //Codigo javascrpt
+    export default{
+
+    }
+</script>
+
+<style>
+    //Estilo
+</style>
