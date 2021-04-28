fce40b62ad25b65a23a776c131a983771bb67f72
Henrique Assis <henrique.assis145@gmail.com>
2021-04-28 20:46:01 -0300

    init modalLogin

---
+++ Dashboard/frontend/dashboard/src/utils/validators.js
@@ -0,0 +1,23 @@
+export function validateEmptyandLength3 (value) {
+  if (!value) {
+    return '*Este campo é obrigatorio'
+  }
+  if (value.length < 3) {
+    return '*Este campo precisa de no minimo 3 caracteres'
+  }
+  return true
+}
+
+export function validateEmptyAndEmail (value) {
+  if (!value) {
+    return '*Este campo é obrigatório'
+  }
+
+  const isValid = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(value)
+
+  if (!isValid) {
+    return '*Este campo precisa ser um e-mail'
+  }
+
+  return true
+}
