58461fbf2fc9aeedcc65c31dd8f49a620853686d
Henrique Assis <henrique.assis145@gmail.com>
2021-05-03 17:57:21 -0300

    leftPanel init

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="pokedex">
-    <h1 class="text--yellow">Pokedex</h1>
+    <Pokedex />
   </div>
 </template>
 
 <script>
+import Pokedex from './components/Pokedex.vue'
 
 export default {
   name: 'App',
+  components:{
+    Pokedex,
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-#pokedex{
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 24px;
-}
+  #pokedex{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height:  calc(100vh - 20px);
+    padding: 24px;
+  }
 </style>
