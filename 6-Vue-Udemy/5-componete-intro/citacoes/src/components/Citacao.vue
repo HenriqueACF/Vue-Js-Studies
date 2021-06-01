be549233fd0f696724d18a427b16086e59c00567
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2021-06-01 15:32:06 -0300

    um components

---
+++ 6-Vue-Udemy/5-componete-intro/citacoes/src/components/Citacao.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log('created')
+    },
+    destroyed(){
+        console.log('destroyed')
+    },
+    activated(){
+        console.log('activated')
+    },
+    deactived(){
+        console.log('deactived')
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    
+</style>
