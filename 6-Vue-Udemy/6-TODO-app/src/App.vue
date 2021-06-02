49da344673f0ab387f984eeac881639033f6b7da
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2021-06-02 09:02:25 -0300

    add localstorage

--- 6-Vue-Udemy/6-TODO-app/src/App.vue
+++ 6-Vue-Udemy/6-TODO-app/src/App.vue
@@ -35,6 +35,15 @@ export default {
 		}
 	},
 
+	watch:{
+		tasks:{
+			deep: true,
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+
 	methods:{
 		addTask(task){
 			const sameName = t => t.name === task.name
@@ -50,6 +59,11 @@ export default {
 		toggleTaskState(i){
 			this.tasks[i].pending = !this.tasks[i].pending
 		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks = Array.isArray(array) ? array : []
 	}
 }
 </script>
