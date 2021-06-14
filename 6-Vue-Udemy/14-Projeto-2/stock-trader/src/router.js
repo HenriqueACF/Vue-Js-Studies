88ef779b26b40ab4d3816337aad73431771d899e
Henrique Assis <henrique.assis145@gmail.com>
2021-06-14 20:02:13 -0300

    init Header

--- 6-Vue-Udemy/14-Projeto-2/stock-trader/src/router.js
+++ 6-Vue-Udemy/14-Projeto-2/stock-trader/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from './components/Home'
+import Home from './components/Home.vue'
 import Portfolio from './components/portfolio/Portfolio'
 import Stocks from './components/stocks/Stocks'
 
