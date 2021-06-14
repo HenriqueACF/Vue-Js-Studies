822248e3a8cbab64edab4beaa948a4bff746b39b
Henrique Assis <henrique.assis145@gmail.com>
2021-06-14 10:04:54 -0300

    vuex

--- 6-Vue-Udemy/13-vuex/vuex-exercicio/src/components/Loja.vue
+++ 6-Vue-Udemy/13-vuex/vuex-exercicio/src/components/Loja.vue
@@ -11,18 +11,28 @@
 </template>
 
 <script>
-import {mapMutations} from 'vuex'
+import {mapActions} from 'vuex'
 
 export default {
     data() {
         return {
             sequencia: 1,
-            quantidade: 1,
-            preco: 9.99,
         }
     },
+    computed:{
+        quantidade(){
+            return this.$store.state.quantidade
+        },
+        preco(){
+            return this.$store.state.preco
+        }
+    },
+
     methods: {
-        ...mapMutations(['adicionarProduto']),
+        ...mapActions(['adicionarProduto']),
+        //adicionarProduto(produto){
+        //    this.$store.dispatch('adicionarProduto', produto)
+        //},
         adicionar() {
             const produto = {
                 id: this.sequencia,
@@ -34,7 +44,8 @@ export default {
 
             //this.$store.state.produtos.push(produto)
             //this.$store.commit('adicionarProduto', produto)
-            this.adicionarProduto(produto)            
+            //this.adicionarProduto(produto)            
+            this.adicionarProduto(produto)
         }
     }
 }
--- 6-Vue-Udemy/13-vuex/vuex-exercicio/src/store/store.js
+++ 6-Vue-Udemy/13-vuex/vuex-exercicio/src/store/store.js
@@ -5,7 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state:{
-    produtos:[]
+    produtos:[],
+    quantidade:2,
+    preco:20.00
   },
 
   getters:{
@@ -18,6 +20,20 @@ export default new Vuex.Store({
   mutations:{
     adicionarProduto(state, payload){
       state.produtos.push(payload)
+    },
+    setQuantidade(state, payload){
+      state.quantidade = payload
+    },
+    setPreco(state, payload){
+      state.preco = payload
+    }
+  },
+
+  actions:{
+    adicionarProduto({commit}, payload){
+      setTimeout(()=>{
+        commit('adicionarProduto', payload)
+      }, 1000)
     }
   }
 })
