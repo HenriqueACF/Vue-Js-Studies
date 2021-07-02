5784426d12a6003329b29ba95af30f4edda396df
Henrique Assis <henrique.assis145@gmail.com>
2021-07-02 15:03:55 -0300

    create components

---
+++ 6-Vue-Udemy/17-ProjetoFinal/frontend/src/components/template/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header'
+}
+</script>
+
+<style>
+  .header{
+    grid-area: header;
+    background: linear-gradient(to right, #1E469A, #49A7C1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
