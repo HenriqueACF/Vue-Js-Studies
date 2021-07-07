20790779e9ffcfc1180eec91d3ccc31da022bbd5
Henrique Assis <henrique.assis145@gmail.com>
2021-07-07 17:31:50 -0300

    Home

--- 6-Vue-Udemy/17-ProjetoFinal/frontend/src/components/home/Home.vue
+++ 6-Vue-Udemy/17-ProjetoFinal/frontend/src/components/home/Home.vue
@@ -5,20 +5,61 @@
       main="Dashboard"
       sub="Base de Conhecimento"
     />
+    <div class="stats">
+      <Stat 
+        title="Categorias" 
+        :value="stat.categories"
+        icon="fa fa-folder"
+        color="#D54D50"
+      />
+      <Stat 
+        title="Artigos" 
+        :value="stat.articles"
+        icon="fa fa-file"
+        color="#3BC480"
+      />  
+      <Stat 
+        title="Usuários" 
+        :value="stat.users"
+        icon="fa fa-user"
+        color="#3282CD"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle.vue'
+import Stat from './Stat.vue'
+import axios from 'axios'
+import {baseApiUrl} from '@/global'
 
 export default {
   name:'Home',
   components:{
-    PageTitle
+    PageTitle,
+    Stat
+  },
+  data: function(){
+    return {
+      stat:{}
+    }
+  },
+  methods:{
+    getStats(){
+      axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
+    }
+  },
+  mounted(){
+    this.getStats()
   }
 }
 </script>
 
 <style>
-
+  .stats{
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 </style>
