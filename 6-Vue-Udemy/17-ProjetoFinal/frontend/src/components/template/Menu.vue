932c796016b08cf979688a99da5250137bdd6be4
HenriqueACF <henrique.assis145@gmail.com>
2021-07-28 09:55:31 -0300

    up

--- 6-Vue-Udemy/17-ProjetoFinal/frontend/src/components/template/Menu.vue
+++ 6-Vue-Udemy/17-ProjetoFinal/frontend/src/components/template/Menu.vue
@@ -41,6 +41,11 @@ export default {
         name:'articlesByCategory',
         params:{id:node.id}
       })
+
+      if(this.$mq === 'xs' || this.$mq === 'sm'){
+        this.$store.commit('toggleMenu', false)
+      }
+
     }
   },
   mounted(){
