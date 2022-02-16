fc2bc3d4ab3aa6e85a336425229ff4239828e0dc
henriqueacf <henrique.assis145@gmail.com>
2022-02-16 11:51:55 -0300

    finish project

--- 7-Projects/recipe-app/src/views/Home.vue
+++ 7-Projects/recipe-app/src/views/Home.vue
@@ -1,44 +1,51 @@
 <template>
   <div class="home">
     <h1>My Recipes</h1>
-    <button>Add new recipes</button>
-    <div class="recipes">
+    <button @click="togglePopup">Add new Recipe</button>
 
+    <div class="recipes">
+      <div class="card" v-for="recipe in $store.state.recipes" :key="recipe.slug">
+        <h2>{{ recipe.title }}</h2>
+        <p>{{ recipe.description }}</p>
+        <router-link :to="`/recipe/${recipe.slug}`">
+          <button>View Recipe</button>
+        </router-link>
+      </div>
     </div>
 
-    <div class="add-recipes-popup">
+    <div class="add-recipe-popup" v-if="popupOpen">
       <div class="popup-content">
-        <h2>Add new recipes</h2>
+        <h2>Add new recipe</h2>
 
-        <form @submit.prevent="">
+        <form @submit.prevent="addNewRecipe">
           <div class="group">
-            <label>Ttile</label>
-            <input type="text">
+            <label>Title</label>
+            <input type="text" v-model="newRecipe.title" />
           </div>
 
           <div class="group">
             <label>Description</label>
-            <textarea></textarea>
+            <textarea v-model="newRecipe.description"></textarea>
           </div>
 
           <div class="group">
             <label>Ingredients</label>
-            <div class="ingredients">
-              <input type="text">
+            <div class="ingredient" v-for="i in newRecipe.ingredientRows" :key="i">
+              <input type="text" v-model="newRecipe.ingredients[i - 1]" />
             </div>
-            <button type="button">Add Ingredient</button>
+            <button type="button" @click="addNewIngredient">Add Ingredient</button>
           </div>
 
           <div class="group">
-            <label>Methods</label>
-            <div class="methods">
-              <textarea></textarea>
+            <label>Method</label>
+            <div class="method" v-for="i in newRecipe.methodRows" :key="i">
+              <textarea v-model="newRecipe.method[i - 1]"></textarea>
             </div>
-            <button type="button">Add Step</button>
+            <button type="button" @click="addNewStep">Add step</button>
           </div>
-          
+
           <button type="submit">Add Recipe</button>
-          <button type="button">Close</button>
+          <button type="button" @click="togglePopup">Close</button>
         </form>
       </div>
     </div>
@@ -46,12 +53,59 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import { ref } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
   name: 'Home',
-  components: {
+  setup () {
+    const newRecipe = ref({
+      title: '',
+      description: '',
+      ingredients: [],
+      method: [],
+      ingredientRows: 1,
+      methodRows: 1
+    });
+    const popupOpen = ref(false);
+    const store = useStore();
+    const togglePopup = () => {
+      popupOpen.value = !popupOpen.value;
+    }
+    const addNewIngredient = () => {
+      newRecipe.value.ingredientRows++;
+    }
     
+    const addNewStep = () => {
+      newRecipe.value.methodRows++;
+    }
+    
+    const addNewRecipe = () => {
+      newRecipe.value.slug = newRecipe.value.title.toLowerCase().replace(/\s/g, '-');
+      if (!newRecipe.value.slug) {
+        alert("Please enter a title");
+        return;
+      }
+      store.commit('ADD_RECIPE', { ...newRecipe.value });
+      newRecipe.value = {
+        title: '',
+        description: '',
+        ingredients: [],
+        method: [],
+        ingredientRows: 1,
+        methodRows: 1
+      };
+      togglePopup();
+    }
+    
+    return {
+      newRecipe,
+      addNewRecipe,
+      togglePopup,
+      popupOpen,
+      addNewStep,
+      addNewIngredient,
+    }
   }
 }
 </script>
